906e89b37bbfc7c7a8a0fd855bec1f4371242a6e
BarMaxx <mbarakhtanov@mail.ru>
2024-01-02 16:12:39 +0300

    1.3.2.1 Beta
    - Изменены некоторые квесты
    - Изменены некоторые рецепты

--- kubejs/server_scripts/dank_storage.js
+++ kubejs/server_scripts/dank_storage.js
@@ -79,4 +79,70 @@ onEvent('recipes', event => {
   })
   
   
+  event.remove({output: 'dankstorage:1_to_2'})	
+  event.shaped('dankstorage:1_to_2', [
+    'RRR',
+    'RSR',
+    'RRR'
+  ], {
+    R: 'compressium:redstone_4',
+    S: 'minecraft:nether_star'
+  })
+  
+  
+  event.remove({output: 'dankstorage:2_to_3'})	
+  event.shaped('dankstorage:2_to_3', [
+    'GGG',
+    'GSG',
+    'GGG'
+  ], {
+    G: 'compressium:gold_5',
+    S: 'minecraft:nether_star'
+  })
+  
+  
+  event.remove({output: 'dankstorage:3_to_4'})	
+  event.shaped('dankstorage:3_to_4', [
+    'EEE',
+    'ESE',
+    'EEE'
+  ], {
+    E: 'compressium:emerald_6',
+    S: 'minecraft:nether_star'
+  })
+  
+  
+  event.remove({output: 'dankstorage:4_to_5'})	
+  event.shaped('dankstorage:4_to_5', [
+    'DDD',
+    'DSD',
+    'DDD'
+  ], {
+    D: 'compressium:diamond_7',
+    S: 'minecraft:nether_star'
+  })
+  
+  
+  event.remove({output: 'dankstorage:5_to_6'})	
+  event.shaped('dankstorage:5_to_6', [
+    'OOO',
+    'OSO',
+    'OOO'
+  ], {
+    O: 'compressium:obsidian_8',
+    S: 'minecraft:nether_star'
+  })
+  
+  
+  event.remove({output: 'dankstorage:6_to_7'})	
+  event.shaped('dankstorage:6_to_7', [
+    'NNN',
+    'NSN',
+    'NNN'
+  ], {
+    N: 'extendedcrafting:nether_star_block',
+    S: 'minecraft:nether_star'
+  })
+  
+  
 })
