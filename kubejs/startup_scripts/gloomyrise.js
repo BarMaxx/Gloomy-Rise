1e70550bbe0a52a1b232e1fce863e350bc711326
BarMaxx <mbarakhtanov@mail.ru>
2024-01-14 14:25:26 +0300

    1.3.2.14 Beta
    - Добавлено несколько новых модов
    - Удалено несколько кастомных предметов
    - Добавлена тестовая часть мультиблока

--- kubejs/startup_scripts/gloomyrise.js
+++ kubejs/startup_scripts/gloomyrise.js
@@ -3,30 +3,15 @@ onEvent('item.registry', event => {
 
 	 event.create('gloomy_rise').displayName('Gloomy Rise')
 	 
-	 event.create('awakened_core').displayName('Пробуждённое ядро')
-	 
-	 event.create('chaos_core').displayName('Ядро хаоса')
-	 
 	 event.create('creative_essence').displayName('Креативная эссенция')
-	 
-	 event.create('draconic_core').displayName('Дракониевое ядро')
-	 
-	 event.create('drill').displayName('Дрель')
 	 
 	 event.create('lamp').displayName('Лампа')
 	 
 	 event.create('lense').displayName('Линза')
 	 
-	 event.create('part_of_cogwheel').displayName('Часть шестерёнки')
-	 
-	 event.create('part_of_water_wheel').displayName('Часть водяного колеса')
-
-	 event.create('part_of_wind_generator').displayName('Часть ветряного генератора')
-	 
 	 event.create('photocell').displayName('Фотоэлемент')
 	 
 	 event.create('raw_circuit').displayName('Сырая схема')
 	 
-	 event.create('Wyvern_core').displayName('Ядро виверны')
 
 })
