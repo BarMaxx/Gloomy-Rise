85babfa0283cf4949bb6110abf815a27080b5826
BarMaxx <98416932+BarMaxx@users.noreply.github.com>
2025-06-13 23:30:50 +0300

    Changed quests & recipes

--- kubejs/server_scripts/recipes/building_gadgets.js
+++ kubejs/server_scripts/recipes/building_gadgets.js
@@ -3,15 +3,54 @@ onEvent('recipes', event => {
 	
   event.remove({output: 'buildinggadgets:gadget_building'})	
   event.shaped('buildinggadgets:gadget_building', [
-    'IRI',
-    'MQM',
-    'ILI'
+    'AEA',
+    'BCB',
+    'ADA'
   ], {
-    I: 'minecraft:iron_ingot',
-    M: 'botania:mana_diamond',
-	Q: 'create:rose_quartz',
-	L: 'minecraft:lapis_lazuli',
-	R: 'minecraft:redstone'
+    A: 'minecraft:iron_ingot',
+    B: 'botania:mana_diamond',
+	C: 'create:rose_quartz',
+	D: 'minecraft:lapis_lazuli',
+	E: 'minecraft:redstone'
+  })
+  
+  
+  event.remove({output: 'buildinggadgets:gadget_exchanging'})	
+  event.shaped('buildinggadgets:gadget_exchanging', [
+    'ACA',
+    'DBD',
+    'ABA'
+  ], {
+    A: 'minecraft:iron_ingot',
+    B: 'minecraft:lapis_lazuli',
+	C: 'minecraft:redstone',
+	D: 'botania:mana_diamond'
+  })
+  
+  
+  event.remove({output: 'buildinggadgets:gadget_copy_paste'})	
+  event.shaped('buildinggadgets:gadget_copy_paste', [
+    'ACA',
+    'DCD',
+    'ABA'
+  ], {
+    A: 'minecraft:iron_ingot',
+    B: 'minecraft:lapis_lazuli',
+	C: 'minecraft:redstone',
+	D: 'botanicalmachinery:mana_emerald'
+  })
+  
+  
+  event.remove({output: 'buildinggadgets:gadget_destruction'})	
+  event.shaped('buildinggadgets:gadget_destruction', [
+    'ACA',
+    'DCD',
+    'ABA'
+  ], {
+    A: 'minecraft:iron_ingot',
+    B: 'minecraft:lapis_lazuli',
+	C: 'minecraft:redstone',
+	D: 'botania:mana_pearl'
   })
   
   
